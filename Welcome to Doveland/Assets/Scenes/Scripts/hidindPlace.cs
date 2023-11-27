725200fffe7a832b6b0f591b44880928f62a4699
Christopher Alvarado <104535558+ChrisA1108@users.noreply.github.com>
2023-11-26 23:05:16 -0600

    AI hiding spot detection/chris a1108 (#14)
    In house to right of spawn location or house 1 a hiding spot was added.
    If player goes up to table a prompt comes up to press e and the player hides and the wulffvook goes back to home location.
    
    * Update shadercompiler-UnityShaderCompiler.exe0.log
    
    * Update shadercompiler-UnityShaderCompiler.exe0.log

---
+++ Welcome to Doveland/Assets/Scenes/Scripts/hidindPlace.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hidingPlace : MonoBehaviour
+{
+    public GameObject hideText, stopHideText;
+    public GameObject normalPlayer, hidingPlayer;
+    public EnemyWulfvook monsterScript;
+    public Transform monsterTransform;
+    bool interactable, hiding;
+    public float loseDistance;
+
+    
+    void Start()
+    {
+        interactable = false;
+        hiding = false;
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("player-camera"))
+        {
+            hideText.SetActive(true);
+            interactable = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("player-camera"))
+        {
+            hideText.SetActive(false);
+            interactable = false;
+        }
+    }
+
+    
+    void Update()
+    {
+        if(interactable == true)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                hideText.SetActive(false);
+                hidingPlayer.SetActive(true);
+                float distance = Vector3.Distance(monsterTransform.position, normalPlayer.transform.position);
+                if(distance > loseDistance)
+                {
+                    if(monsterScript.playerHiding == false)
+                    {
+                        monsterScript.stopChase();
+                    }
+                }
+                stopHideText.SetActive(true);
+                hiding = true;
+                normalPlayer.SetActive(false);
+                interactable = false;
+            }
+        }
+        if(hiding == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                stopHideText.SetActive(false);
+                normalPlayer.SetActive(true);
+                hidingPlayer.SetActive(false);
+                hiding = false;
+                monsterScript.startChase();
+                
+            }
+        }
+    }
+}
